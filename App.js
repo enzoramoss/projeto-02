337d6d7ccc2b68139c6eb3ec92834fba08d65841
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2026-03-26 00:33:06 +0000

    mini alterações

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-import { StyleSheet, View } from 'react-native';
-import { useState } from 'react';
-
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-import styles from './styles';
-
 import Login from './components/Login';
 import CadastroDeClientes from './components/CadastroDeClientes';
-import ListaProdutos from './components/ListaProdutos';
 import Carrinho from './components/Carrinho';
 import AreaAdmin from './components/AreaAdmin';
 
@@ -23,32 +16,8 @@ export default function App() {
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={CadastroDeClientes} />
         <Stack.Screen name='AdminArea' component={AreaAdmin} />
-        <Stack.Screen name='Products' component={ListaProdutos} />
         <Stack.Screen name='Cart' component={Carrinho} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-// <View style={styles.container}>
-//   {!registered ?
-//     (<Login obj={{ register: registered }}
-//       objLogged={{ logged: logged }}
-//       setLogged={setLogged}
-//       setRegistered={setRegistered}
-//       usersStorage={AsyncStorage}
-//       adminLogged={{ adminLog: adminLog }}
-//       setAdminLogged={setAdminLog} />)
-//     :
-//     (<CadastroDeClientes
-//       setRegistered={setRegistered}
-//       obj={{ register: registered }}
-//       AsyncStorage={AsyncStorage} />
-//     )
-//   }
-
-//   {logged && <ListaProdutos />}
-
-//   {adminLog && <AreaAdmin />}
-// </View>
-
